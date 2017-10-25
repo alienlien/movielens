fc2fd0cad10a1c57db25717c8bb5f14c1978cfb9
Alien Lien <alienlien@gmail.com>
2017-10-26 03:39:47 +0800

    Use logging instead print in training script

--- train_rec.py
+++ train_rec.py
@@ -2,12 +2,16 @@
 #
 # This is used to train a recommendation system based on
 # collaborative filtering.
+import logging
 import os.path
-from time import gmtime, strftime
 from docopt import docopt
 import numpy as np
 import pandas as pd
 import surprise as sp
+
+# Setup logging format.
+logging.basicConfig(
+    format='%(asctime)s:[%(levelname)s]:%(message)s', level=logging.INFO)
 
 # Default number of n-folded.
 NUM_FOLD = 3
@@ -40,14 +44,10 @@ if __name__ == '__main__':
         movies[['movieId', 'title']].set_index('movieId'),
         on='movieId').reset_index()
 
-    print('>> Begin to read data')
-    print(strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime()))
+    logging.info('Begin to read data: {}'.format(movie_csv_file))
     reader = sp.Reader(rating_scale=(1, 5))
     data = sp.Dataset.load_from_df(df[['userId', 'movieId', 'rating']], reader)
     data.split(n_folds=NUM_FOLD)
-
-    print('>> Begin to train data')
-    print(strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime()))
 
     sim_options = {
         'name': 'pearson_baseline',
@@ -55,12 +55,12 @@ if __name__ == '__main__':
     }
     alg = sp.KNNBaseline(sim_options=sim_options)
     counter = 0
+    logging.info('Begin to enter the training procedure.')
     for train_set, test_set in data.folds():
-        print('>> Training {}/{} data'.format(counter, NUM_FOLD))
-        print(strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime()))
+        logging.info('Training {}/{} data'.format(counter, NUM_FOLD))
         alg.train(train_set)
-        print('>> Evaluating {}/{} data'.format(counter, NUM_FOLD))
-        print(strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime()))
+
+        logging.info('Evaluating {}/{} data'.format(counter, NUM_FOLD))
         sp.accuracy.rmse(alg.test(test_set), verbose=True)
         counter += 1
 
